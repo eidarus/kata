73d16f50738ed7fc08f42e74341b6bc85ba2baf3
eidarus omar <eida@eidas-mbp.mynetworksettings.com>
2022-08-25 18:15:44 -0400

    Task 1 logic

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,3 +1,16 @@
 class Calculator < ApplicationRecord
-  
+  def initialize(numbers)
+    @numbers = numbers
+  end
+
+  def add
+    return 0 if @numbers == ""
+    sum = 0
+    @numbers.split(',').each do |x|
+        sum = sum + x.to_i
+    end
+    
+    return sum
+  end
+
 end
