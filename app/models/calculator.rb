9ca4c52e74cb8af2218e877cfc7a325ab063dbd0
eidarus omar <eidaruso@gmail.com>
2022-08-25 18:31:17 -0400

    Task 4 logic

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -5,12 +5,19 @@ class Calculator < ApplicationRecord
 
   def add
     return 0 if @numbers == ""
+    delim = check_delimiter(@numbers)
     sum = 0
-    @numbers.split(',').each do |x|
+    @numbers.split("#{delim}").each do |x|
         sum = sum + x.to_i
     end
-    
+
     return sum
+  end
+
+  def check_delimiter(numbers)
+    return "," if numbers.first != "/"
+    
+    return numbers[2]
   end
 
 end
